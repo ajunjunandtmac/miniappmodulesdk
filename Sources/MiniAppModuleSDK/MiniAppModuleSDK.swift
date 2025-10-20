b0fdf753c8755ef57ff1107892ad39d34e27a560
zhou_jun <zhou_jun@internal.dev>
2025-10-20 10:30:05 +0800

    Add UIViewController Type Constraints

--- Sources/MiniAppModuleSDK/MiniAppModuleSDK.swift
+++ Sources/MiniAppModuleSDK/MiniAppModuleSDK.swift
@@ -6,12 +6,13 @@ import UIKit
 import SwiftUI
 
 public protocol MiniAppModule {
+    associatedtype MiniAppBaseViewController: UIViewController
     /// 模块唯一标识符
     static var moduleID: String { get }
 
-    static func getInstance() -> MiniAppModule
+    static func getInstance() -> any MiniAppModule
     /// UIKit入口
-    func createInitialViewController() -> UIViewController?
+    func createInitialViewController() -> MiniAppBaseViewController?
     /// SwiftUI入口（可选）
     func createInitialView() -> AnyView?
 }
