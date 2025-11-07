bec159063ba0a230099603ec3e658e9ca2813c29
zhou_jun <zhou_jun@internal.dev>
2025-11-07 16:47:26 +0800

    SDK bumped to stable version

--- Sources/MiniAppModuleSDK/MiniAppModuleSDK.swift
+++ Sources/MiniAppModuleSDK/MiniAppModuleSDK.swift
@@ -6,22 +6,64 @@ import UIKit
 import SwiftUI
 
 public protocol MiniAppModule {
-    /// 模块唯一标识符
-    static var moduleID: String { get }
+    /// mini app id
+    static var miniAppId: String { get }
 
-    static func getInstance(withModuleId id: String) -> (any MiniAppModule)?
-    /// UIKit入口
+    static func getInstance(withMiniAppId id: String) -> (any MiniAppModule)?
+    /// UIKit entrance
     func createInitialViewController() -> UIViewController?
-    /// SwiftUI入口（可选）
+    /// SwiftUI entrance
     func createInitialView() -> AnyView?
 }
 
-public class MiniAppModuleCreator {
-    public static func createModule(moduleId: String, moduleName: String, miniAppName: String) -> (any MiniAppModule)? {
-        guard let ClassType = NSClassFromString("\(miniAppName).\(moduleName)") as? MiniAppModule.Type else {
+public struct MiniAppInfo {
+    let miniAppId: String
+    let libraryName: String
+
+    public init(miniAppId: String, libraryName: String) {
+        self.miniAppId = miniAppId
+        self.libraryName = libraryName
+    }
+}
+
+public final class MiniAppModuleManager: @unchecked Sendable {
+    public static let shared = MiniAppModuleManager()
+    private init() {}
+
+    public func getViewController(with info: MiniAppInfo) -> UIViewController? {
+        let moduleInstance = getModuleInstance(with: info)
+        return moduleInstance?.createInitialViewController()
+    }
+
+    public func getSwiftView(with info: MiniAppInfo) -> AnyView? {
+        let moduleInstance = getModuleInstance(with: info)
+        return moduleInstance?.createInitialView()
+    }
+
+    public func isModuleActive(with module: MiniAppInfo) -> Bool {
+        return getModuleInstance(with: module) != nil
+    }
+
+    private func getModuleInstance(with info: MiniAppInfo) -> (any MiniAppModule)? {
+        let miniAppId = info.miniAppId
+        let libraryName = info.libraryName
+        return MiniAppModuleCreator.createModule(miniAppId: miniAppId, libraryName: libraryName)
+    }
+}
+
+final class MiniAppModuleCreator {
+
+    /// Create mini app module instance by reflection
+    /// - Parameters:
+    ///   - moduleId: the mini app ID
+    ///   - libraryName: the mini app library name
+    /// - Returns: the instance of the mini app module
+    static func createModule(miniAppId: String, libraryName: String) -> (any MiniAppModule)? {
+        let nameSpace = libraryName
+        guard let ClassType = NSClassFromString("\(nameSpace).\(libraryName)") as? MiniAppModule.Type else {
             return nil
         }
 
-        return ClassType.getInstance(withModuleId: moduleId)
+        return ClassType.getInstance(withMiniAppId: miniAppId)
     }
 }
